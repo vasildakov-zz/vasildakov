89ad384e4e3d84359f1e220bf109130777803ec0
Vasil Dakov <vasildakov@gmail.com>
2015-08-18 21:05:42 -0400

    Created Photography module

---
+++ module/Photography/config/module.config.php
@@ -0,0 +1,54 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'photography' => array(
+                'type'    => 'Hostname',
+                'options' => array(
+                    'route'    => 'photography.vasildakov.dev',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Photography\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Photography\Controller\Index' => 'Photography\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/photography.phtml',
+            'application/index/index' => __DIR__ . '/../view/photography/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
